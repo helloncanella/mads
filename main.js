71f6f8095d824a97918730abae40c6ed7df5cf9e
Hellon Canella <helloncanella@gmail.com>
2016-01-12 18:11:34 +0000

    trianglify is perfect

--- main.js
+++ main.js
@@ -1,8 +1,13 @@
+/*global Trianglify*/
+
 var mosaic = $('#mosaic');
 var palettes = ['YlGn','YlGnBu','GnBu','BuGn','PuBuGn','PuBu','BuPu','RdPu','PuRd','OrRd','YlOrRd','YlOrBr','Purples','Blues','Greens','Oranges','Reds','Greys','PuOr','BrBG','PRGn','PiYG', 'RdBu','RdGy','RdYlBu','Spectral','RdYlGn'];
 var colorItems = ['q0-9','q1-9','q2-9','q3-9','q4-9','q5-9','q6-9','q7-9','q8-9']; 
+var selectedPalette;
 
-$('input').slider();
+
+
+start();
 
 $('button').click(function (e) {
   e.preventDefault();
@@ -16,13 +21,15 @@ $('button').click(function (e) {
   if(id=='original'){
     original();
   }else if (id=='trianglify'){
+    mosaic.css('background','none');
+    $('#trianglify-box').css('visibility','visible');
     trianglify();
   }
 
 });
 
-generatePalettes();
 
+generatePalettes();
 
 function generatePalettes(){
   
@@ -39,19 +46,69 @@ function generatePalettes(){
       $('.color-item').css('height', width);
     
     });
-    
-    
 
+  });
+  
+  $('.palette').click(function(){
+    $(this).siblings().css('border','none');
+    $(this).css('border','3px solid #484848');
+    
+    selectedPalette = $(this).data('palette');
+    trianglify(selectedPalette);
   });
   
   
 }
 
+function start(){
+  $('#original').css('background','#E6E6E6');
+  $('#settings').click(function(e){
+    $('#config').toggleClass('show-slide');
+  });
+  original();
+  config();
+}
 
 function original (){
   mosaic.css('background',"url('assets/wall.jpg')");
+  $('#trianglify-box').css('visibility','hidden');
 }
 
 function trianglify (){
-  mosaic.css('background','royalblue');
+  mosaic.html('');
+ 
+  var options = {
+    height: $(window).height(),
+    width: $(window).width(),
+    cell_size: $('input#cell-size').slider('getValue'),
+    variance: $('input#variance').slider('getValue'),
+    x_colors: selectedPalette || 'YlOrBr'
+  }
+
+  mosaic.append(Trianglify(options).canvas());
+}
+
+function configPaletteSelector(){
+  $('input#variance').slider({
+    min:0,
+    max:1,
+    value:0.50,
+    precision:2,
+    step:0.01
+  });
+
+  $('input#cell-size').slider({
+    min:10,
+    max:400,
+    value:100,
+    step:5
+  });
+  
+  $('#trianglify-box *').on('slide',function(e){
+    trianglify();
+  });
+}
+
+function config (){
+  configPaletteSelector()
 }
